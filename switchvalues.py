371ea1e883b1d97b159260bbd11fe2ea0374e5f2
Ashley <Ashhamilton22888@gmail.com>
2023-11-18 10:34:38 -0500

    first commit

---
+++ switchvalues.py
@@ -0,0 +1,16 @@
+# write a program that takes two inputs. and switched the values stored in variables a and b
+
+# There are two variables, a and b from input 
+a=input("What value do you want for a? ")
+b=input("What value do you want for b? ")
+
+# dont change the code above 
+# write your code below this line 
+x=a
+a=b
+b=x
+
+
+# write your code above this line 
+print("a: "+a)
+print("b: "+b)
